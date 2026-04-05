01c17a930e3d2955526e5b021ab7f2b648572021
Sahas Subramanian <sahas.subramanian@proton.me>
2026-04-06 01:13:02 +0200

    Move binary to src/bin/, and make src/ into a library

--- src/bin/microsim.rs
+++ src/bin/microsim.rs
@@ -1,9 +1,4 @@
-mod lisp;
-mod proto;
-mod server;
-mod timeout_tracker;
-
-use proto::microgrid::v1alpha18::microgrid_server;
+use microsim::proto::microgrid::v1alpha18::microgrid_server;
 use std::env;
 use tonic::transport::Server;
 
@@ -18,28 +13,27 @@ async fn main() {
     // If no argument provided, the "./config.lisp" is used as default
     let first_arg = args.next().unwrap_or("./config.lisp".to_string());
 
-    let load_path = if first_arg == "--help" {
-            println!("
-                Usage: microsim [--help] [CONFIG_PATH]
+    if first_arg == "--help" {
+        println!(
+            r#"Usage: microsim [--help] [CONFIG_PATH]
 
-                Options:
-                  --help  Show this help and exit
+Options:
+  --help  Show this help and exit
 
-                Args:
-                  CONFIG_PATH  Path to the config file
-            ");
-            return;
-        } else {
-            first_arg
-    };
+Args:
+  CONFIG_PATH  Path to the config file
+"#
+        );
+        return;
+    }
+    let load_path = first_arg;
 
-
-    let config = lisp::Config::new(&load_path);
+    let config = microsim::lisp::Config::new(&load_path);
     tokio::spawn(config.clone().start());
     let socket_addr = config.socket_addr();
     log::info!("Server listening on {}", socket_addr);
 
-    let server = server::MicrogridServer::new(config);
+    let server = microsim::server::MicrogridServer::new(config);
     Server::builder()
         .add_service(microgrid_server::MicrogridServer::new(server))
         .serve(socket_addr.parse().unwrap())
