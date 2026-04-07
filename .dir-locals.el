e379c3f32170a3370847850c42173080eb57169f
Sahas Subramanian <sahas.subramanian@proton.me>
2026-04-07 13:44:11 +0200

    Improve automatic tags table generation
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,15 @@
 ((lisp-mode . ((eval . (progn
                          (if (not (boundp 'tulisp-etags-setup-done))
-                             (progn
+                             (let* ((project-root-dir (expand-file-name (project-root (project-current))))
+                                   (tags-file (expand-file-name "TAGS" project-root-dir)))
                                (setq-local tulisp-etags-setup-done t)
-                               (tags-reset-tags-tables)
+
+                               (if (file-exists-p tags-file)
+                                   (delete-file tags-file))
+
                                ;; Set up etags-regen for this project
                                (setq-local etags-regen-program "cargo")
                                (setq-local etags-regen-program-options
-                                           `("run" "--bin" "microsim-etags" ,(expand-file-name (project-root (project-current)))))
-                               (etags-regen-mode 1))))))))
+                                           `("run" "-q" "--bin" "microsim-etags" ,project-root-dir))
+                               (etags-regen-mode 1)
+                               (tags-reset-tags-tables))))))))
